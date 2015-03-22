9178f15ffb1ef9a023599c648bb592c403db40c2
eeveeayen <evanlws12@gmail.com>
2015-03-22 19:01:16 -0400

    Fixed floyd's

--- src/assignment3/Floyd.java
+++ src/assignment3/Floyd.java
@@ -1,20 +1,18 @@
 package assignment3;
-import model.Graph;
 
 public class Floyd {
 
-	public static int[][] transitiveClosure(Graph graph, int [][] theMatrix){
-		int [][] matrix = theMatrix;
-		for(int n = 1; n < graph.numNodes + 1; n++){
-			for(int m = 1; m < graph.numNodes + 1; n++){
+	public static int[][] transitiveClosure(int [][] matrix){
+		for(int n = 1; n < matrix.length + 1; n++){
+			for(int m = 1; m < matrix.length + 1; m++){
 				if((n != m) && (matrix[n][m] == 0)){
 					matrix[n][m] = Integer.MAX_VALUE;
 				}
 			}
 		}
-		for(int k = 1; k < graph.numNodes + 1; k++){
-			for(int i = 1; i < graph.numNodes + 1; i++){
-				for(int j = 1; j < graph.numNodes + 1; j++ ){
+		for(int k = 1; k < matrix.length + 1; k++){
+			for(int i = 1; i < matrix.length + 1; i++){
+				for(int j = 1; j < matrix.length + 1; j++ ){
 						matrix[i][j] = Math.min(matrix[i][j], (matrix[i][k] + matrix[k][j]));
 				}
 			}
