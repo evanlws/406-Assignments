3427898bc44e2539e77cb9a079ce00fc2de113e9
eeveeayen <evanlws12@gmail.com>
2015-03-22 20:31:06 -0400

    knapsack

--- src/assignment3/Knapsack.java
+++ src/assignment3/Knapsack.java
@@ -2,20 +2,23 @@ package assignment3;
 
 public class Knapsack {
 
-	public Knapsack(int [] v,int [] w, int n, int W){
+	public static int[][] knapsackSubset(int [] v,int [] w, int n, int W) {
 
 		int [][] M = new int[n+1][W + 1];
 		for(int j = 0; j < W; j++){
 			M[0][j] = 0;
 		}
-		for(int i = 0; i < n ; i++){
+		for(int i = 1; i < n ; i++){
 			for(int j = 0; j < W ; j++){
 				if((j - w[i]) >= 0) {
 					M[i][j] = Math.max( M[i-1][j] , (v[i] + M[i-1][j - w[i]] ));
 				} else {
+					System.out.println(i + " " + j);
 					M[i][j] = M[i-1][j];
 				}
 			}
 		}
+		return M;
 	}
+
 }
