0b0fe496e34709a9b5a115aa5cc6de453eb65a5f
eeveeayen <evanlws12@gmail.com>
2015-03-23 20:42:33 -0400

    updated matrix multiplication

--- src/assignment3/MatrixMultiplication.java
+++ src/assignment3/MatrixMultiplication.java
@@ -1,21 +1,23 @@
 package assignment3;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MatrixMultiplication {
 
 	protected static String str = "";
+	protected static int [][] N;
 	
 	public static int[][] matrixMultiplicationProblem(int n, int [] di) {
-		int [][] N = new int[n][n];
-		
+		N = new int[n][n];
+
 		for(int i = 0; i < (n - 1); i++){
 			N[i][i] = 0;
 		}
 		for(int b = 1; b < (n-1); b++){
 			for(int i = 0; i < (n-b-1); i++){
 				int j = i + b;
-				PriorityQueue<int[][]> que = new PriorityQueue<int[][]>();
+				PriorityQueue<int[][]> que = new PriorityQueue<int[][]>(1, theComparator);
 				for(int k = i; k < (j-1); k++){
 					int number = N[i][k] + N[k + 1][j] + (di[i]*di[k + 1] * di[j + 1]);
 					int [][] kValues = new int [1][2];
@@ -32,21 +34,26 @@ public class MatrixMultiplication {
 		printTheOrder(0,firstNumber);
 		printTheOrder(firstNumber + 1, N.length - 1);
 		System.out.println("Parentesis matrices: " + str);
-		
 		return N;
 	}
-	
-	/*protected static Comparator<int[][]> theComparator = new Comparator<int[][]>(){
+
+	protected static Comparator<int[][]> theComparator = new Comparator<int[][]>(){
 		public int compare(int[][] obj1, int [][] obj2){
 			return (int) (obj1[0][0] - obj2[0][0]);
 		}
-	};*/
-	
+	};
+
 	protected static void printTheOrder(int n, int m){
 		if(n == m){
-			str += "A[" + n + "]";
+			str += "A[" + n + "]";	
 		}
-		
+		else{
+			str += " (";
+			printTheOrder(n, N[m][n]);
+			printTheOrder(N[m][n] + 1, m);
+			str += ") ";
+		}
+
 	}
 
 }
