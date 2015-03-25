f407c6a3af945970154c460f68c197e2fc0f85f6
eeveeayen <evanlws12@gmail.com>
2015-03-25 00:15:32 -0400

    FINAL

--- src/assignment3/Warshall.java
+++ src/assignment3/Warshall.java
@@ -4,9 +4,9 @@ public class Warshall {
 
 	public static int[][] transitiveClosure(int [][] theMatrix) {
 		int [][] matrix = theMatrix;
-		for(int k = 1; k < theMatrix.length + 1; k++) {
-			for(int i = 1; i < theMatrix.length + 1; i++) {
-				for(int j = 1; j < theMatrix.length + 1; j++) {
+		for(int k = 1; k < theMatrix.length; k++) {
+			for(int i = 1; i < theMatrix.length; i++) {
+				for(int j = 1; j < theMatrix.length; j++) {
 					if((matrix [i][k] == 1) && (matrix[k][j] == 1)) {
 						matrix[i][j] = 1;
 					}
